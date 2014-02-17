245921dc59555dae008c2d08bba305b8ec5407d2
Tom Curran <tom@tomcurran.org>
2014-02-17 00:08:24 +0000

    UI utility for parsing text views

---
+++ android/src/main/java/org/tomcurran/remiges/util/UIUtils.java
@@ -0,0 +1,27 @@
+package org.tomcurran.remiges.util;
+
+
+import android.text.TextUtils;
+import android.widget.TextView;
+
+public class UIUtils {
+
+    public final static int DEFAULT_PARSE_TEXTVIEW_INT = 0;
+
+    public static int parseTextViewInt(TextView view) {
+        return parseTextViewInt(view, DEFAULT_PARSE_TEXTVIEW_INT);
+    }
+
+    public static int parseTextViewInt(TextView view, int alt) {
+        String text = view.getText().toString();
+        if (TextUtils.isEmpty(text)) {
+            return alt;
+        }
+        try {
+            return Integer.parseInt(text);
+        } catch (NumberFormatException e) {
+            return alt;
+        }
+    }
+
+}
