b628868004d255b2a608678bc6ae28e918253762
Tom Curran <tom@tomcurran.org>
2014-03-24 11:26:58 +0000

    remove dependencies between tests. text fields are cleared before setting the value fixes #10
    - generate different test data
    - tests add & remove their data
    - removed numbers that encouraged ordering
    - made comments more generic

--- android/src/androidTest/java/org/tomcurran/remiges/test/ui/JumpTypeTestCase.java
+++ android/src/androidTest/java/org/tomcurran/remiges/test/ui/JumpTypeTestCase.java
@@ -18,9 +18,13 @@ public class JumpTypeTestCase extends RemigesUiAutomatorTestCase {
     private static final String RESOURCE_ACTION_DELETE = "org.tomcurran.remiges:id/menu_jumptype_detail_delete";
 
     private static final String NAME_HINT = "Jump Type";
-    private static final String NAME1 = "jump type tester 1";
-    private static final String NAME2 = "jump type tester 2";
-    private static final String NAME3 = "jump type tester 3";
+    private static final String NAME = "jump type tester %d";
+
+    private int nameCount = 0;
+
+    private String getNextJumpTypeName() {
+        return String.format(NAME, ++nameCount);
+    }
 
     public static void navigateToJumpType(String navigateFrom) throws UiObjectNotFoundException {
         NavigationDrawerTestCase.navigateTo(navigateFrom, TEXT_NAVIGATION, TEXT_TITLE);
@@ -59,11 +63,29 @@ public class JumpTypeTestCase extends RemigesUiAutomatorTestCase {
         assertInList(getMasterDetailList(), jumpTypeName);
     }
 
-    public void test010NavigateTo() throws UiObjectNotFoundException {
+    private void deleteJumpType(String jumpTypeName) throws UiObjectNotFoundException {
+        // count items in list
+        int listItemsBefore = getMasterDetailListCount();
+
+        // click item
+        clickListItem(getMasterDetailList(), jumpTypeName);
+
+        // ensure correct page
+        assertEquals(isTwoPane() ? TEXT_TITLE : TEXT_DETAIL_TITLE, getActionBarTitle());
+
+        // click delete action
+        getByResource(RESOURCE_ACTION_DELETE).clickAndWaitForNewWindow();
+
+        // ensure item removed from list
+        assertEquals(listItemsBefore - 1, getMasterDetailListCount());
+        assertNotInList(getMasterDetailList(), jumpTypeName);
+    }
+
+    public void testNavigateTo() throws UiObjectNotFoundException {
         navigateToJumpType();
     }
 
-    public void test011NavigateAwayAndBack() throws UiObjectNotFoundException {
+    public void testNavigateAwayAndBack() throws UiObjectNotFoundException {
         // navigate to jump type
         JumpTypeTestCase.navigateToJumpType();
 
@@ -74,100 +96,118 @@ public class JumpTypeTestCase extends RemigesUiAutomatorTestCase {
         JumpTypeTestCase.navigateToJumpType(PlacesTestCase.DESCRIPTION_OPEN_DRAWER);
     }
 
-    public void test012AddCancel() throws UiObjectNotFoundException {
-        // navigate to jump type
+    public void testAddCancel() throws UiObjectNotFoundException {
+        // navigate
         navigateToJumpType();
 
-        // get current amount of jump types available
+        // count items in list
         int listItemsBefore = getMasterDetailListCount();
 
-        // click add jump type action
+        // click add action
         getByDescription(DESCRIPTION_ADD).clickAndWaitForNewWindow();
 
         UiObject name = getByResource(RESOURCE_EDIT_NAME);
-        // ensure jump type name is set to the hint
+        // ensure name is set to the hint
         assertEquals(NAME_HINT, name.getText());
 
         // action bar cancel
         getByResource(RESOURCE_ACTIONBAR_CANCEL).clickAndWaitForNewWindow();
 
-        // ensure no new jump type in list
+        // ensure no new item in list
         assertEquals(listItemsBefore, getMasterDetailListCount());
     }
 
-    public void test013Add() throws UiObjectNotFoundException {
-        addJumpType(NAME1);
+    public void testAdd() throws UiObjectNotFoundException {
+        String jumpTypeName = getNextJumpTypeName();
+
+        // add item
+        addJumpType(jumpTypeName);
+
+        // delete item
+        deleteJumpType(jumpTypeName);
     }
 
-    public void test014EditCancel() throws UiObjectNotFoundException {
-        // navigate to jump type
-        navigateToJumpType();
+    public void testEditCancel() throws UiObjectNotFoundException {
+        String jumpTypeName = getNextJumpTypeName();
 
-        // click jump type
-        clickListItem(getMasterDetailList(), NAME1);
+        // add item
+        addJumpType(jumpTypeName);
+
+        // click item
+        clickListItem(getMasterDetailList(), jumpTypeName);
 
         // ensure correct page
         assertEquals(isTwoPane() ? TEXT_TITLE : TEXT_DETAIL_TITLE, getActionBarTitle());
 
-        // ensure detail name
-        assertEquals(NAME1, getByResource(RESOURCE_DETAIL_NAME).getText());
+        // ensure detail
+        assertEquals(jumpTypeName, getByResource(RESOURCE_DETAIL_NAME).getText());
 
-        // click edit jump type action
+        // click edit action
         getByResource(RESOURCE_ACTION_EDIT).clickAndWaitForNewWindow();
 
         // action bar cancel
         getByResource(RESOURCE_ACTIONBAR_CANCEL).clickAndWaitForNewWindow();
 
-        // ensure detail name
-        assertEquals(NAME1, getByResource(RESOURCE_DETAIL_NAME).getText());
+        // ensure detail
+        assertEquals(jumpTypeName, getByResource(RESOURCE_DETAIL_NAME).getText());
+
+        // ensure correct view
+        if (!isTwoPane()) {
+            getUiDevice().pressBack();
+        }
+
+        // delete item
+        deleteJumpType(jumpTypeName);
     }
 
-    public void test015Edit() throws UiObjectNotFoundException {
-        // navigate to jump type
-        navigateToJumpType();
+    public void testEdit() throws UiObjectNotFoundException {
+        String jumpTypeName = getNextJumpTypeName();
 
-        // click jump type
-        clickListItem(getMasterDetailList(), NAME1);
+        // add item
+        addJumpType(jumpTypeName);
+
+        // click item
+        clickListItem(getMasterDetailList(), jumpTypeName);
 
         // ensure correct page
         assertEquals(isTwoPane() ? TEXT_TITLE : TEXT_DETAIL_TITLE, getActionBarTitle());
 
-        // ensure detail name
-        assertEquals(NAME1, getByResource(RESOURCE_DETAIL_NAME).getText());
+        // ensure detail
+        assertEquals(jumpTypeName, getByResource(RESOURCE_DETAIL_NAME).getText());
 
-        // click edit jump type action
+        // click edit action
         getByResource(RESOURCE_ACTION_EDIT).clickAndWaitForNewWindow();
 
-        // change jump type name
+        // change name
         UiObject name = getByResource(RESOURCE_EDIT_NAME);
-        name.setText(NAME2);
-        assertEquals(NAME2, name.getText());
+        name.clearTextField();
+        jumpTypeName = getNextJumpTypeName();
+        name.setText(jumpTypeName);
+        assertEquals(jumpTypeName, name.getText());
 
         // action bar done
         getByResource(RESOURCE_ACTIONBAR_DONE).clickAndWaitForNewWindow();
 
-        // ensure detail name
-        assertEquals(NAME2, getByResource(RESOURCE_DETAIL_NAME).getText());
+        // ensure detail
+        assertEquals(jumpTypeName, getByResource(RESOURCE_DETAIL_NAME).getText());
+
+        // ensure correct view
+        if (!isTwoPane()) {
+            getUiDevice().pressBack();
+        }
+
+        // delete item
+        deleteJumpType(jumpTypeName);
     }
 
-    public void test016Delete() throws UiObjectNotFoundException {
-        addJumpType(NAME3);
+    public void testDelete() throws UiObjectNotFoundException {
+        String jumpTypeName = getNextJumpTypeName();
 
-        // get current amount of jump types available
-        int listItemsBefore = getMasterDetailListCount();
+        // add item
+        addJumpType(jumpTypeName);
 
-        // click jump type
-        clickListItem(getMasterDetailList(), NAME3);
-
-        // ensure correct page
-        assertEquals(isTwoPane() ? TEXT_TITLE : TEXT_DETAIL_TITLE, getActionBarTitle());
-
-        // click delete jump type action
-        getByResource(RESOURCE_ACTION_DELETE).clickAndWaitForNewWindow();
-
-        // ensure jump type removed from list
-        assertEquals(listItemsBefore - 1, getMasterDetailListCount());
-        assertNotInList(getMasterDetailList(), NAME3);
+        // delete item
+        deleteJumpType(jumpTypeName);
     }
 
 }
